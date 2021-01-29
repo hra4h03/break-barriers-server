41e6878eb4ba56a00471f3b68db3a1800ae71f38
Hrach2003 <hrachya.hovakimyan.03@mail.ru>
2021-01-29 10:02:34 +0400

    combine ui

--- client/src/components/dash/dash.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const Dash = ({ ...props }: { [x: string]: unknown }): JSX.Element => (
-  <hr
-    className="rounded-full dark:bg-gray-300 bg-gray-900 h-1 divide-dashed mx-auto my-4"
-    {...props}
-  />
-);
