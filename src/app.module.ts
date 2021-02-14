77f91bc658ce0a8cb6b51aa49c3e76552d1dfdc6
Hrach2003 <hrachya.hovakimyan.03@mail.ru>
2021-02-14 10:48:28 +0400

    import export fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,6 @@ import { MongooseSchemasModule } from './mongoose-schema/mongoose-schema.module'
 import { AuthModule } from './auth/auth.module';
 import { AdminPanelModule } from './admin/admin.module';
 import { MailModule } from './mail/mail.module';
-import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [
@@ -28,9 +27,8 @@ import { MessagesModule } from './messages/messages.module';
     MongooseSchemasModule,
     AuthModule,
     MailModule,
-    MessagesModule,
   ],
   controllers: [AppController],
-  providers: [AppService, ],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Message, MessageSchema } from './entities/message.entity';
 import { MessagesService } from './messages.service';
 
 @Module({
-  providers: [MessagesService]
+  providers: [MessagesService],
+  imports: [
+    MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
+  ],
+  exports: [MessagesService],
 })
 export class MessagesModule {}
